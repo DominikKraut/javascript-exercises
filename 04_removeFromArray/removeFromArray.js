ee76ca07753c039bb715abc807aeb7ffcf47f115
Dominik Placr <dominik.placr@applifting.cz>
2022-11-04 15:25:11 +0100

    Completed 04, needed help

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...args) {
+  let items = arr;
+  const valuesToRemove = Array.from(args);
+  return (items = items.filter((i) => !valuesToRemove.includes(i)));
 };
 
 // Do not edit below this line
